b0521793b8d602af2e15d5791a97ebf9c33f1063
franck <carnet.franck.paul@gmail.com>
2023-06-20 15:40:58 +0200

    Use dcCore::app()->adminurl->redirect() rather than Http::redirect()

--- src/Manage.php
+++ src/Manage.php
@@ -25,7 +25,6 @@ use Dotclear\Helper\Html\Form\Para;
 use Dotclear\Helper\Html\Form\Submit;
 use Dotclear\Helper\Html\Form\Text;
 use Dotclear\Helper\Html\Html;
-use Dotclear\Helper\Network\Http;
 use Exception;
 
 class Manage extends dcNsProcess
@@ -62,7 +61,7 @@ class Manage extends dcNsProcess
                 dcCore::app()->blog->triggerBlog();
 
                 dcPage::addSuccessNotice(__('Settings have been successfully updated.'));
-                Http::redirect(dcCore::app()->admin->getPageURL());
+                dcCore::app()->adminurl->redirect('admin.plugin.' . My::id());
             } catch (Exception $e) {
                 dcCore::app()->error->add($e->getMessage());
             }
