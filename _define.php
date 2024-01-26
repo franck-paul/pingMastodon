a647149b60bbfbc548c5e06fd1c2cc52323f5ce0
franck <carnet.franck.paul@gmail.com>
2024-01-26 10:14:23 +0100

    New release 4.3

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Ping Mastodon',
     'Ping Mastodon',
     'Franck Paul',
-    '4.2',
+    '4.3',
     [
         'requires'    => [['core', '2.28']],
         'type'        => 'plugin',
