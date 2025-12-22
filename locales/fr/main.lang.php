1675762d695b78abb8854f889b00396c05275171
franck <carnet.franck.paul@gmail.com>
2025-12-22 10:45:41 +0100

    French locales

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -30,6 +30,7 @@ L10n::$locales['Mastodon instance:'] = 'Instance Mastodon :';
 L10n::$locales['Application token:'] = 'Jeton applicatif :';
 L10n::$locales['Status prefix:'] = 'Préfixe de statut :';
 L10n::$locales['Status visibility:'] = 'Visibilité du statut :';
+L10n::$locales['Automatically ping when an entry is first published'] = 'Ping automatique à la première publication d\'une entrée';
 L10n::$locales['Catchphrase'] = 'Phrase d\'accroche';
 L10n::$locales['Use entry catchphrase if available'] = 'Utiliser la phrase d\'accroche de l\'entrée si elle est disponible';
 L10n::$locales['The catchphrase is defined for each entry, see the options when creating/editing it.'] = 'La phrase d\'accroche est définie pour chaque entrée, voir les options lors de la création/modification.';
